193d45444bc87e3f3f43f7095082241568dea754
Moyano1711 <moyanoc-j@javeriana.edu.co>
2023-11-14 18:32:48 -0500

    fix-model

--- parcialfrontend/src/app/model/equipo.model.ts
+++ parcialfrontend/src/app/model/equipo.model.ts
@@ -1,11 +1,13 @@
 export class Equipo{
   id : number
   nombre : string
+  ciudad : string
   copasInternacionales :  number
 
-  constructor(id : number, nombre:string, copasInternacionales:number) {
+  constructor(id : number, nombre:string, ciudad:string, copasInternacionales:number) {
     this.id = id
     this.nombre = nombre
+    this.ciudad = ciudad
     this.copasInternacionales = copasInternacionales
   }
 }
